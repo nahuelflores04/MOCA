0f70b741c7da0a571f89a74389ee692e9a2ad30e
Nahuel Flores <89166495+nahuelflores04@users.noreply.github.com>
2023-03-30 19:25:47 -0300

    Añadiendo handlebars como framework para render de vistas

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import app from "./infrastructure/web/index.js";
 
-
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 
 app.listen(port, ()=>{
     console.log('Server on port:', port)
--- src/infrastructure/web/index.js
+++ src/infrastructure/web/index.js
@@ -1,11 +1,61 @@
 import express from 'express';
+import path from "path";
+import { fileURLToPath } from "url";
+import { dirname } from "path";
+import cors from "cors";
+import { engine } from "express-handlebars";
 
 const app = express()
 
 //Set middlewars, statics, use, etc
 
-app.get('/', (req,res)=>{
-    res.send('SERVER OK')
+
+const __filename = fileURLToPath(import.meta.url);
+export const __dirname = dirname(__filename);
+const publicPath = path.join(__dirname, "public");
+
+app.use(express.static(publicPath));
+app.use(cors());
+
+//Handlebars
+app.set("views", path.join(__dirname, "../../views"));
+app.engine(
+  ".hbs",
+  engine({
+    helpers: {
+      iff: function (a, operator, b, opts) {
+        let bool = false;
+
+        switch (operator) {
+          case `===`:
+            bool = a == b;
+            break;
+          case `>`:
+            bool = a > b;
+            break;
+          case `<`:
+            bool = a < b;
+            break;
+          default:
+            bool = a == b;
+        }
+        if (bool) {
+          return opts.fn(this);
+        }
+        return opts.inverse(this);
+      },
+    },
+
+    defaultLayout: "main",
+    layoutsDir: path.join(app.get("views"), "/layouts"),
+    partialsDir: path.join(app.get("views"), "/partials"),
+    extname: ".hbs",
+  })
+);
+app.set("view engine", ".hbs");
+
+app.get('/', (req,res)=>{ //=> Este get moverlo luego a routes.
+    res.render('index')
 });
 
 export default app
